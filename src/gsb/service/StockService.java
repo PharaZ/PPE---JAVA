1f763587b2e79c17f7c07b733cad6fbb376251f0
Icenuke <ths.nico@gmail.com>
2015-11-10 10:53:06 +0100

    StockService

--- src/gsb/service/StockService.java
+++ src/gsb/service/StockService.java
@@ -1,10 +1,22 @@
 package gsb.service;
+import gsb.modele.Stock;
+import gsb.modele.dao.StockDao;
 
 public class StockService {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	//Recherche le depot legal d'un medicament dans Stock.
+	public static Stock rechercherStock(String unDepotLegalMedicament){
+		Stock unStock = null;
+		try{
+		if (unDepotLegalMedicament==null) {
+            throw new Exception("Donn�e obligatoire : Depot Legal");
+        }
+		unStock = StockDao.rechercher(unDepotLegalMedicament);
+		}
+		catch(Exception e){
+			System.out.println( e.getMessage());
+		}
+		return unStock;
 	}
 
 }
