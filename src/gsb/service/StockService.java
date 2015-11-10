6c96a5ecd44647825d28a0cebddda5be5a35a9c2
Icenuke <ths.nico@gmail.com>
2015-11-10 11:56:52 +0100

    Modification service

--- src/gsb/service/StockService.java
+++ src/gsb/service/StockService.java
@@ -1,4 +1,5 @@
 package gsb.service;
+import gsb.modele.Medicament;
 import gsb.modele.Stock;
 import gsb.modele.dao.StockDao;
 
@@ -18,5 +19,15 @@ public class StockService {
 		}
 		return unStock;
 	}
+	public static int creerMedicament(String depotLegal, String nonomCommercialm, String composition, String effets, String contreIndication,
+			 float prixEchantillon, String codeFamille, String libelleFamille){
+		Medicament unMedicament;
+		int resultat = 0;
+		
+		try{ // controle des param�tres d'entr�e
+			 // Si le donn�es est manquante on l�ve une exception
+	        if (depotLegal==null || nomCommercial == null || composition == null || effets == null || contreIndication == null || prixEchantillon == null || codeFamille == null || libelleFamille == null) {
+	            throw new Exception("Donn�es obligatoires : depot legal, nom commercial, composition, effets, contre indication, prix echantillon, code famille,  libelle famille");
 
 }
+		}
